74715e29dce88a5d1a4ea1edc66bb3395cc8af9b
ivan260666 <vaniash84@icloud.com>
2023-02-01 02:34:49 +0300

    Range part 2

--- Range/src/ru/academits/shalnov/range/Range.java
+++ Range/src/ru/academits/shalnov/range/Range.java
@@ -34,4 +34,67 @@ public class Range {
 
         return number - from >= -epsilon && number - to <= epsilon;
     }
+
+    public Range getCrossingRange(Range range2) {
+        double rangeStart1 = this.getFrom();
+        double rangeEnd1 = this.getTo();
+
+        double rangeStart2 = range2.getFrom();
+        double rangeEnd2 = range2.getTo();
+
+        double crossingRangeStart = Math.max(rangeStart1, rangeStart2);
+        double crossingRangeEnd = Math.min(rangeEnd1, rangeEnd2);
+
+        if ((this.isInside(crossingRangeStart) && this.isInside(crossingRangeStart)) &&
+                (range2.isInside(crossingRangeStart) && range2.isInside(crossingRangeEnd)) &&
+                (crossingRangeStart != crossingRangeEnd)) {
+            return new Range(crossingRangeStart, crossingRangeEnd);
+        }
+
+        return null;
+    }
+
+    public Range[] getRangeIntervalsUnion(Range range2) {
+        double rangeStart1 = this.getFrom();
+        double rangeEnd1 = this.getTo();
+
+        double rangeStart2 = range2.getFrom();
+        double rangeEnd2 = range2.getTo();
+
+        double crossingRangeStart = Math.max(rangeStart1, rangeStart2);
+        double crossingRangeEnd = Math.min(rangeEnd1, rangeEnd2);
+
+        if ((this.isInside(crossingRangeStart) && this.isInside(crossingRangeStart)) &&
+                (range2.isInside(crossingRangeStart) && range2.isInside(crossingRangeEnd))) {
+            return new Range[]{new Range(Math.min(rangeStart1, rangeStart2), Math.max(rangeEnd1, rangeEnd2))};
+        }
+
+        return new Range[]{new Range(rangeStart1, rangeEnd1), new Range(rangeStart2, rangeEnd2)};
+    }
+
+    public Range[] getDifferenceBetweenRanges(Range subtractedRange) {
+        double rangeStart = this.getFrom();
+        double rangeEnd = this.getTo();
+
+        double subtrahendRangeStart = subtractedRange.getFrom();
+        double subtrahendRangeEnd = subtractedRange.getTo();
+
+        if (subtractedRange.isInside(rangeStart) && subtractedRange.isInside(rangeEnd)) {
+            return null;
+        }
+
+        if (subtractedRange.isInside(rangeStart)) {
+            return new Range[]{new Range(subtrahendRangeEnd, rangeEnd)};
+        }
+
+        if (subtractedRange.isInside(rangeEnd)) {
+            return new Range[]{new Range(rangeStart, subtrahendRangeStart)};
+        }
+
+        if (this.getCrossingRange(subtractedRange) != null) {
+            return new Range[]{new Range(rangeStart, subtrahendRangeStart), new Range(subtrahendRangeEnd, rangeEnd)};
+        }
+
+        return new Range[]{new Range(rangeStart, rangeEnd)};
+    }
 }
--- Range/src/ru/academits/shalnov/range_main/Main.java
+++ Range/src/ru/academits/shalnov/range_main/Main.java
@@ -8,42 +8,90 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введите начало диапазона:");
-        double rangeStart = scanner.nextDouble();
+        System.out.println("Введите начало диапазона 1:");
+        double rangeStart1 = scanner.nextDouble();
 
-        System.out.println("Введите конец диапазона:");
-        double rangeEnd = scanner.nextDouble();
+        System.out.println("Введите конец диапазона 1:");
+        double rangeEnd1 = scanner.nextDouble();
 
-        Range range = new Range(rangeStart, rangeEnd);
+        Range range1 = new Range(rangeStart1, rangeEnd1);
 
-        System.out.println("Начало диапазона = " + range.getFrom());
-        System.out.println("Конец диапазона = " + range.getTo());
-        System.out.println("Длина диапазона = " + range.getLength());
+        System.out.println("Начало диапазона 1 = " + range1.getFrom());
+        System.out.println("Конец диапазона 1 = " + range1.getTo());
+        System.out.println("Длина диапазона 1 = " + range1.getLength());
 
-        System.out.println("Введите точку для проверки:");
-        double point1 = scanner.nextDouble();
+        System.out.println("Введите начало диапазона 2:");
+        double rangeStart2 = scanner.nextDouble();
+
+        System.out.println("Введите конец диапазона 2:");
+        double rangeEnd2 = scanner.nextDouble();
+
+        Range range2 = new Range(rangeStart2, rangeEnd2);
+
+        System.out.println("Начало диапазона 2 = " + range2.getFrom());
+        System.out.println("Конец диапазона 2 = " + range2.getTo());
+        System.out.println("Длина диапазона 2 = " + range2.getLength());
+
         System.out.println();
 
-        if (range.isInside(point1)) {
-            System.out.println("Точка принадлежит диапазону");
+        Range crossingRange = range1.getCrossingRange(range2);
+
+        if (crossingRange == null) {
+            System.out.println("У заданных диапазонов нет пересечения");
         } else {
-            System.out.println("Точка не принадлежит диапазону");
+            System.out.println("Начало диапазона пересечения = " + crossingRange.getFrom());
+
+            System.out.println("Конец диапазона пересечения = " + crossingRange.getTo());
         }
 
         System.out.println();
 
-        System.out.println("Введите новое начало диапазона:");
-        rangeStart = scanner.nextDouble();
+        Range[] arrayUnionRanges = range1.getRangeIntervalsUnion(range2);
+        System.out.println("Количество диапазонов полученых в результате объединения = " + arrayUnionRanges.length);
 
-        System.out.println("Введите новый конец диапазона:");
-        rangeEnd = scanner.nextDouble();
-
-        range.setFrom(rangeStart);
-        range.setTo(rangeEnd);
+        for (int i = 0; i < arrayUnionRanges.length; i++) {
+            System.out.println("Начало диапазона " + (i + 1) + " = " + arrayUnionRanges[i].getFrom());
+            System.out.println("Конец диапазона " + (i + 1) + " = " + arrayUnionRanges[i].getTo());
+        }
 
         System.out.println();
-        System.out.println("Новое начало диапазона = " + range.getFrom());
-        System.out.println("Новый конец диапазона = " + range.getTo());
-        System.out.println("Новая длина диапазона = " + range.getLength());
+
+        if (range1.getDifferenceBetweenRanges(range2) == null) {
+            System.out.println("Количество диапазонов полученых в результате разности = 0");
+        } else {
+            Range[] arrayDifferenceRanges = range1.getDifferenceBetweenRanges(range2);
+            System.out.println("Количество диапазонов полученых в результате разности = " + arrayDifferenceRanges.length);
+
+            for (int i = 0; i < arrayDifferenceRanges.length; i++) {
+                System.out.println("Начало диапазона " + (i + 1) + " = " + arrayDifferenceRanges[i].getFrom());
+                System.out.println("Конец диапазона " + (i + 1) + " = " + arrayDifferenceRanges[i].getTo());
+            }
+        }
+
+        System.out.println("Введите точку для проверки:");
+        double point1 = scanner.nextDouble();
+
+        if (range1.isInside(point1)) {
+            System.out.println("Точка принадлежит диапазону 1");
+        } else {
+            System.out.println("Точка не принадлежит диапазону 1");
+        }
+
+        System.out.println();
+
+        System.out.println("Введите новое начало диапазона 1:");
+        rangeStart1 = scanner.nextDouble();
+
+        System.out.println("Введите новый конец диапазона 1:");
+        rangeEnd1 = scanner.nextDouble();
+
+        range1.setFrom(rangeStart1);
+        range1.setTo(rangeEnd1);
+
+        System.out.println();
+
+        System.out.println("Новое начало диапазона 1 = " + range1.getFrom());
+        System.out.println("Новый конец диапазона 1 = " + range1.getTo());
+        System.out.println("Новая длина диапазона 1 = " + range1.getLength());
     }
 }
