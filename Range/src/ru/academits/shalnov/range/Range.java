c248579017890de0e4c7ee4cb519c50585d2da99
ivan260666 <vaniash84@icloud.com>
2023-01-31 19:23:25 +0300

    Range part 1

--- Range/src/ru/academits/shalnov/range/Range.java
+++ Range/src/ru/academits/shalnov/range/Range.java
@@ -1,4 +1,37 @@
 package ru.academits.shalnov.range;
 
 public class Range {
+    private double from;
+    private double to;
+
+    public Range(double from, double to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public double getFrom() {
+        return from;
+    }
+
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
+    public double getLength() {
+        return to - from;
+    }
+
+    public boolean isInside(double number) {
+        double epsilon = 1e-10;
+
+        return number - from >= -epsilon && number - to <= epsilon;
+    }
 }
--- Range/src/ru/academits/shalnov/range_main/Main.java
+++ Range/src/ru/academits/shalnov/range_main/Main.java
@@ -1,7 +1,49 @@
 package ru.academits.shalnov.range_main;
 
+import ru.academits.shalnov.range.Range;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите начало диапазона:");
+        double rangeStart = scanner.nextDouble();
+
+        System.out.println("Введите конец диапазона:");
+        double rangeEnd = scanner.nextDouble();
+
+        Range range = new Range(rangeStart, rangeEnd);
+
+        System.out.println("Начало диапазона = " + range.getFrom());
+        System.out.println("Конец диапазона = " + range.getTo());
+        System.out.println("Длина диапазона = " + range.getLength());
+
+        System.out.println("Введите точку для проверки:");
+        double point1 = scanner.nextDouble();
+        System.out.println();
+
+        if (range.isInside(point1)) {
+            System.out.println("Точка принадлежит диапазону");
+        } else {
+            System.out.println("Точка не принадлежит диапазону");
+        }
+
+        System.out.println();
+
+        System.out.println("Введите новое начало диапазона:");
+        rangeStart = scanner.nextDouble();
+
+        System.out.println("Введите новый конец диапазона:");
+        rangeEnd = scanner.nextDouble();
+
+        range.setFrom(rangeStart);
+        range.setTo(rangeEnd);
+
+        System.out.println();
+        System.out.println("Новое начало диапазона = " + range.getFrom());
+        System.out.println("Новый конец диапазона = " + range.getTo());
+        System.out.println("Новая длина диапазона = " + range.getLength());
     }
 }
